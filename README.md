5936b7988e5b8f960d0380007ed65bdc1c0be5f9
Bruh Beyene <bbeyene@pdx.edu>
2020-10-16 22:53:05 -0700

    Cleaned up, commented, added readme

---
+++ README.md
@@ -0,0 +1,36 @@
+# HW 1 - Monopoles
+## Bruh Beyene
+
+A complete depth-first search that attempts to place m monopoles (1…m) into n rooms, where no pairs add to equal another in the same room, e.g. if (1, 2) were in room 1, then (3) must be in another room.
+
+I started by studing Bart Massey's 'Sliding Tile Puzzle Solver' which used a dfs. Here, no state information needs to be saved explicitly because no state can be arrived to while traversing rooms. 
+
+I started with the following on paper...
+
+
+while current != n
+	depth = 0
+	room = 0
+	if compatible
+		inDepth = 0
+		place current
+		current++
+		depth++
+		inDepth++
+	room++
+	inDepth--
+
+... which isn't correct at all but it showed me that I should be tracking the state of the room configuration and not the traversal of the rooms. Then, as I drew out a tree, I saw that the depth was the monopole count...
+
+If n = 2 and the rooms are in order:
+
+at depth 1: [ 1 ][  ]
+at depth 2: [ 1 ][ 2 ] , [ 1,2 ][  ]
+at depth 3: [ 1,3 ][ 2 ] , [ 1 ][ 2,3 ] , [ 1,2,3 ][  ] , [ 1,2 ][ 3 ]
+.
+.
+.
+
+So, the dfs stack became the possible rooms while the depth was the current monopole count. When all of the rooms were exhausted with no branch having a solution, it removes the monopole (back up a level) and tries the next room of the branch above.
+
+What is left is seeing if a heuristic exists.
--- monodfs.py
+++ monodfs.py
@@ -1,11 +1,18 @@
-import argparse
+#!/usr/bin/python3
+# Solution for part one of Monopoles found at https://www.drdobbs.com/monopoles/184411053
+# Bruh Beyene
+# CS441 Artificial Intelligence HW1
+import sys
 
+# A list of lists (rooms of monopoles) with dfs solver
 class Monopoles(object):
     def __init__(self, m, n):
         self.m = m 
         self.n = n
         self.rooms = [[] for i in range(n)]
 
+    # Checks if a number can belong in a room
+    # True if X + Y doesn't equal candidate
     def compatible(self, mono, room):
         for i in self.rooms[room]:
             for j in self.rooms[room]:
@@ -14,32 +21,52 @@ class Monopoles(object):
                         return False 
         return True
 
+    # With help from Bart Massey's code at https://github.com/pdx-cs-ai/slider
+    # Difference being no state saving because a state cannot be had more than once
+    # A monopole is placed at each turn therefore mono == depth
     def dfs(self, mono):
         mono = mono + 1
         if mono > self.m:
-            print("solved")
+            # The last monopole was placed compatibly -> done
             return []
 
+        # List of rooms a monople is allowed in given a room's current monopoles
         possible = [i for i in range(self.n) if self.compatible(mono, i)]
 
+        # Try the monopole in each room
         for i in possible:
             self.rooms[i].append(mono)
+            # Add to room and keep deepening until the last monopole is sat
             soln = self.dfs(mono)
             if soln != None:
                 return [i] + soln
+            # Not sat so remove from room to try another
             self.rooms[i].pop()
 
+        # not satisfied in any rooms
         return None
 
+    def show(self, soln):
+        print("")
+        if soln == None:
+            print("unsat")
+        else:
+            for i in range(self.n):
+                row = ""
+                for j in range(len(self.rooms[i])):
+                    row += str(self.rooms[i][j]) + " "
+                print(row)
+        print("")
+
+    # solve using dfs starting with monopole 0
     def solve(self):
         soln = self.dfs(0)
-        if soln != None:
-            print(soln)
+        self.show(soln)
 
-parser = argparse.ArgumentParser(description='Monopoles Sover.')
-parser.add_argument('-m', type=int, help='monopoles')
-parser.add_argument('-n', type=int, help='rooms')
-args = parser.parse_args()
+# m: number of monopoles
+# n: number of rooms
+m = int(sys.argv[1])
+n = int(sys.argv[2])
 
-monopoles = Monopoles(args.m, args.n)
+monopoles = Monopoles(int(m), int(n))
 monopoles.solve()
