cf6b4d106c72f96ea2edbeec31654d31f0c5676e
Bruh Beyene <bbeyene@pdx.edu>
2020-11-08 21:51:23 -0800

    added test

--- unmonosat.py
+++ unmonosat.py
@@ -13,6 +13,21 @@ from sys import argv, stdin
 M = int(argv[1]) # number of monopoles
 N = int(argv[2]) # number of rooms
 
+# Test function: Check if a number can be in a room
+def compatible(mono, room):
+    for i in rooms[room]:
+        for j in rooms[room]:
+            if (i + j) == mono:
+                if i != j:
+                    return False 
+    return True
+
+# Test function: Make sure solution is valid (no X + Y = Z in any room unless X = Y)
+def test_soln(rooms):
+    for i in range(N):
+        for j in range(len(rooms[i])):
+            assert compatible(rooms[i][j], i)
+
 # Extract monopole and room number
 def locate(s):
     s -= 1
@@ -27,24 +42,28 @@ if header == "SAT":
     # Borrowed from Bart Massey's sudoku-sat-py/sudokusolve.py
     # Discard negatives
     solution = [int(b) for b in next(stdin).split() if int(b) > 0]
-    
-    rooms = [[] for n in range(N)]
 
+    rooms = [[] for n in range(N)]
+    
     # Place satisfied monopoles into rooms
     for s in solution:
         (m, n) = locate(s)
         rooms[n].append(m)
 
-    # Print rooms
+    test_soln(rooms)
+
+    # Show the rooms
     for i in range(N):
         row = ""
         for j in range(len(rooms[i])):
             row += str(rooms[i][j]) + " "
         print(row)
 
+
 elif header == "UNSAT":
     print(header)
 else:
     print("invalid solution: not minisat format")
 print()
+
 
