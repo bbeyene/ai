380469fe8499106aa511232868132e450b798d65
Bruh Beyene <bbeyene@pdx.edu>
2020-11-26 23:35:12 -0800

    idk what i'm doing

--- features.py
+++ features.py
@@ -1,5 +1,5 @@
 
-import os, random, sys
+import os, math, sys
 
 # Read the words of each paragraph from the four novels into memory
 def read_novel(fname):
@@ -20,10 +20,8 @@ def read_novel(fname):
         return pars
 
 
-
-
 # Strip away leading and trailing punctuation and remove internal punctuation
-# Words of one letter should probably be ignored
+# Words less than 4 letters ignored
 # Reduce to set of unique words present in the paragraph
 def clean_construct(novel, corpus_words):
 
@@ -36,7 +34,7 @@ def clean_construct(novel, corpus_words):
         allwords = set()
         for sentence in paragraph:
             words = sentence.replace('--', ' ').split()
-            justwords = {alphas(w) for w in words}
+            justwords = {alphas(w) for w in words if len(w) > 4}
             allwords |= justwords
             allwords -= discard
             corpus_words |= allwords
@@ -68,19 +66,58 @@ def label(fname, novel):
     return labeled
 
 
-# Calculate the information gain of splitting the paragraphs on w using the equations above. 
+# Calculate the information gain of splitting the paragraphs on w using entropy equations
 def calc_word_gains(words, paragraphs):
-    """
-    prNpos = # of paragraphs w is in / number of paragraphs
-    prNneg = 1 - prNpos
 
-    U = -(prN * math.log(prN) + (1 - prN) * math.log(1 - prN))
-    """
+    # of pars w is in / number of paragraphs
+    # U = -(prN * math.log(prN) + (1 - prN) * math.log(1 - prN))
+
+    total_pars = 0
+    for nov in paragraphs:
+        total_pars += len(nov)
+
     word_gains = dict()
-    for w in corpus_words:
-        for nov in labeled_paragraphs:
+    for w in words:
+        authors = [0] * 2
+        for nov in paragraphs:
             for par in nov:
-                word_gains[w] = random.randint(1, 100)
+                if w in par:
+                    if w in word_gains.keys():
+                        word_gains[w] += 1
+                    else:
+                        word_gains[w] = 1
+                    authors[int(par[1])] += 1
+    
+        # feature appears
+        prPos = word_gains[w] / len(paragraphs)
+
+        # feature doesn't appear
+        prNeg = 1 - prPos
+
+        # probability author is 0 where word appears
+        prPos_auth0 = authors[0] / (authors[0] + authors[1])
+        # probability author is 1 where word appears
+        prPos_auth1 = authors[1] / (authors[0] + authors[1])
+        # probability author is 0 where word doesn't appear
+        prNeg_auth0 = 1 - prPos_auth0
+        # probability author is 1 where word doesn't appear
+        prNeg_auth1 = 1 - prPos_auth1
+
+        # class entropy for "word appears"
+        try: lg_1 = math.log(prPos_auth1, 2)
+        except: lg_1 = 0
+        try: lg_0 = math.log(prPos_auth0, 2)
+        except: lg_0 = 0
+        uPos = - prPos_auth1 * lg_1 - prPos_auth0 * lg_0 
+
+        # class entropy for "word doesn't appear"
+        try: lg_1 = math.log(prNeg_auth1, 2)
+        except: lg_1 = 0
+        try: lg_0 = math.log(prNeg_auth0, 2)
+        except: lg_0 = 0
+        uNeg = - prNeg_auth1 * lg_1 - prNeg_auth0 * lg_0 
+        G = prPos * uPos + prNeg * uNeg
+        word_gains[w] = G
 
     return word_gains
 
@@ -89,9 +126,13 @@ def calc_word_gains(words, paragraphs):
 # paragraph identifier, paragraph class, and the values of the 300 features
 def output(words, paragraphs):
     vectors = list()
-
-    print(words)
-
+    for w in words:
+        print(w)
+    """
+    for nov in paragraphs:
+        for par in nov:
+            print(words)
+    """
 
 novels = [] # each novel has hundreds of paragraphs
 cleaned_novels = [] # cleaned paragraphs
@@ -102,15 +143,15 @@ features = [] # the nfeatures highest-gain words to use as features of the parag
 labeled_paragraphs = [] # a list of lists of comma-separated: paragraph identifier, the paragraph class, and the unique words per paragraph
 
 for fname in sys.argv[1:]:
-    novel = read_novel(fname)
-    cleaned = clean_construct(novel, corpus_words)
-    cleaned_novels.append(cleaned)
-    labeled = label(fname, cleaned)
-    labeled_paragraphs.append(labeled)
-    # labeled_paragraphs.append(label(fname, clean_construct(read_novel(novel), corpus_words)))
+    #novel = read_novel(fname)
+    #cleaned = clean_construct(novel, corpus_words)
+    #cleaned_novels.append(cleaned)
+    #labeled = label(fname, cleaned)
+    #labeled_paragraphs.append(labeled)
+    labeled_paragraphs.append(label(fname, clean_construct(read_novel(fname), corpus_words)))
 
 word_gains = calc_word_gains(corpus_words, labeled_paragraphs)
-for w, g in sorted(word_gains.items(), key=lambda item: item[1])[0:nfeatures]:
-    features.append(w)
+features = sorted(word_gains.items(), key=lambda item: item[1], reverse=True)
+#features = sorted(word_gains.items(), key=lambda item: item[1], reverse=True)[0:nfeatures]
 
 output(features, labeled_paragraphs)
